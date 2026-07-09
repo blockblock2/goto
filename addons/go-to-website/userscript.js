722873940eb8bd26992c40e16a697c2fd180b125
mateobaraldi16-design <mateobaraldi16@gmail.com>
2026-07-09 17:09:56 -0400

    Remove broken createCategory

--- addons/go-to-website/userscript.js
+++ addons/go-to-website/userscript.js
@@ -1,13 +1,5 @@
 export default async function ({ addon }) {
-  addon.tab.createCategory({
-    id: "go-to-website",
-    name: "Go To Website",
-    color: "#0066ff",
-    icon: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='white' d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z'/%3E%3C/svg%3E"
-  });
-
   addon.tab.addBlock("go to website %s", {
-    categoryId: "go-to-website",
     args: ["URL"],
     displayName: "go to website [URL]",
     callback: ({ URL }) => {
@@ -18,7 +10,6 @@ export default async function ({ addon }) {
   });
 
   addon.tab.addBlock("open website %s in new tab", {
-    categoryId: "go-to-website",
     args: ["URL"],
     displayName: "open website [URL] in new tab",
     callback: ({ URL }) => {
